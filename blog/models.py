ec3b6c01a7b92ef24771c65c26a716cfcb1013fd
Eduardo Nicastro <enicastro@fullstacklabs.co>
2021-11-03 21:57:35 -0300

    add Author and Post models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=100)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now_add=True)
+    slug = models.SlugField(default="", blank=True, null=False, db_index=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
+    content = models.TextField()
+
+    def get_absolute_url(self) -> str:
+        return reverse("post-detail", args=[self.slug])
+
+    def __str__(self) -> str:
+        return self.title
